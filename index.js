c3e2a233547bd8ea9081b950ef1b6c2a18cde022
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-04-10 22:09:26 -0400

    completed objects lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Edwin',
+    streetAddress: "123 Main Street"
+}
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value){
+        const newEmployee = {
+            ...employee};
+
+             newEmployee[key] = value;
+           return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
+        employee[key] = value;
+        return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+      const newDeleted = {
+        ...employee}
+        delete newDeleted[key];
+        return newDeleted;
+    
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
